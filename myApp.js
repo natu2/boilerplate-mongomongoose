6d44dce4e05ef23da8a7ddff459159d3b22ec096
natu2 <93054275+natu2@users.noreply.github.com>
2024-07-25 05:06:57 +0000

    Created MongoDB Atlas database and connected.

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
+/** # MONGOOSE SETUP
+ *  ==============   */
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 let Person;
 
